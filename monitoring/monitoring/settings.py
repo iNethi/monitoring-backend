a2805179a572b2e5f893a4969d66610ba4058341
Keegan White <keeganthomaswhite@gmail.com>
2025-03-23 19:22:52 +0200

    added cors app to settings

--- monitoring/monitoring/settings.py
+++ monitoring/monitoring/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     "django_celery_beat",
     # inethi apps
     'accounts',
